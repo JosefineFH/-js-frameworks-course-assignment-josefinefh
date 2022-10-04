cc8e0ba09505e58ebf29c9fa16c71388eed84d82
Josefine Magnussen <josefine.magnussen@hotmail.com>
2022-10-04 19:54:59 +0200

    created files

---
+++ src/components/Navigation/Navbar.js
@@ -0,0 +1,20 @@
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+function Navigation() {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+export default Navigation;
