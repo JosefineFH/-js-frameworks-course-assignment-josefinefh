4e26863aee129605a7fc7fafd650f1fcd0564140
Josefine Magnussen <josefine.magnussen@hotmail.com>
2022-10-11 18:58:00 +0200

    add paragraf

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -4,6 +4,9 @@ import GetFromLocalStorage from "./CheckLogedIn";
 export default function Admin(){
   GetFromLocalStorage();
   return(
+    <div className="container">
     <Heading title="Admin Page"/>
+    <p>You are now loged in and your token is in the local storage. </p>
+    </div>
   )
 }
